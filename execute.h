c078f322c280304c4da42cab962336efec9de466
lautaro deco <lautaro.deco@mi.unc.edu.ar>
2024-09-02 00:02:53 -0300

    me falto actualizar algunos archivos

--- execute.h
+++ execute.h
@@ -17,4 +17,26 @@ void execute_pipeline(pipeline apipe);
  * Requires: apipe!=NULL
  */
 
+void execute_command(scommand cmd);
+//Función auxiliar para ejecutar un comando simple
+
+
+char **scommand_to_argv(scommand cmd);
+//Función auxiliar para crear un arreglo de cadena de caracteres para así poder usar execvp dado las exigencias de su sintaxis
+
+
+void free_memory_scommand_to_argv(char**killme);
+//Función auxiliar para liberar la memoria de scommand_to_argv
+
+
+void execute_with_pipe(scommand cmd, int input_fd, int output_fd);
+//Función auxiliar para ejecutar un comando con redirección de entrada/salida y pipes
+
+
+void execute_background(pipeline apipe);
+// Función auxiliar para manejar la ejecución en segundo plano
+
+void setup_redirection(scommand cmd);
+// Función auxiliar para configurar redirecciones de entrada y salida
 #endif /* EXECUTE_H */
+
